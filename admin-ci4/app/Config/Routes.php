4fbbc8d1cdfe28171de11a8a63eb8845785f0670
Danendra Utama <danendanendra06@gmail.com>
2026-06-15 16:58:03 +0700

    fix(admin-ci4): update routing from transactions to borrows

--- admin-ci4/app/Config/Routes.php
+++ admin-ci4/app/Config/Routes.php
@@ -18,11 +18,11 @@ $routes->group('', ['filter' => 'auth:admin'], static function($routes) {
     $routes->resource('users', ['controller' => 'Users']);
     $routes->post('users/(:num)/suspend', 'Users::suspend/$1');
     $routes->post('users/(:num)/activate', 'Users::activate/$1');
-    $routes->get('transactions', 'Transactions::index');
-    $routes->match(['GET', 'POST'], 'transactions/new', 'Transactions::create');
-    $routes->get('transactions/returns', 'Transactions::returns');
-    $routes->match(['GET', 'POST'], 'transactions/process_return/(:num)', 'Transactions::processReturn/$1');
-    $routes->get('transactions/(:num)', 'Transactions::show/$1');
+    $routes->get('borrows', 'Borrows::index');
+    $routes->match(['GET', 'POST'], 'borrows/new', 'Borrows::create');
+    $routes->get('borrows/returns', 'Borrows::returns');
+    $routes->match(['GET', 'POST'], 'borrows/process_return/(:num)', 'Borrows::processReturn/$1');
+    $routes->get('borrows/(:num)', 'Borrows::show/$1');
     $routes->resource('fines');
     $routes->post('fines/mark_paid/(:num)', 'Fines::markPaid/$1');
     $routes->post('fines/mark_unpaid/(:num)', 'Fines::markUnpaid/$1');
